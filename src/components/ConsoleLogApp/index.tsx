8fa0f6bf386b04ca15646ccc0b4c25e7584bcfe4
k123s456h <k123s456h@gmail.com>
2022-05-09 21:34:51 +0900

    *^*

--- src/components/ConsoleLogApp/index.tsx
+++ src/components/ConsoleLogApp/index.tsx
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { isMobile, MobileView } from "react-device-detect";
 
 export default function ConsoleLogApp()
 {
   const LIMIT = 7;
   const [log, setLog] = useState<string[]>([]);
+  const buffer = useRef<string[]>([]);
 
-  const originalLog = console.log;
-  console.log = function(...args){
-    originalLog.apply(this, args);
-    setLog([...args.slice(0, LIMIT), ...log.slice(0, LIMIT - args.length >= 0 ? LIMIT - args.length : 0)]);
+  if(isMobile)
+  {
+    console.log = function(...args){
+      buffer.current = [...buffer.current, ...args];
+    }
   }
 
 
+  useEffect(() => {
+    setLog([...buffer.current, ...log.slice(0, LIMIT - buffer.current.length > 0 ? LIMIT - buffer.current.length : buffer.current.length)]);
+    buffer.current = [];
+  }, [log]);
+
+
   return (
-    <div style={{position: 'fixed', left: 0, bottom: '0px', backgroundColor: 'white'}}>
-      <b>last {LIMIT} console log message:</b> <br/>
-      {
-        log.map((msg, idx) => (
-          <div key={idx}>{msg}<br/></div>
-        ))
-      }
-    </div>
+    <MobileView>
+      <div style={{position: 'fixed', left: 0, bottom: '0px', backgroundColor: 'white'}}>
+        <b>last {LIMIT} console log message:</b> <br/><hr/>
+        {
+          log.map((msg, idx) => (
+            <div key={idx}>{msg}<br/></div>
+          ))
+        }
+      </div>
+    </MobileView>
   );
 }
