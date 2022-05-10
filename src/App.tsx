c3dc51925f10c77fc16aafa59b6947ed12ed96fa
k123s456h <k123s456h@gmail.com>
2022-05-10 19:49:38 +0900

    >_<

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,13 @@ function App() {
     width: document.body.clientWidth,
     height: document.body.clientHeight,
   });
+  const [renderChildren, setRenderChildren] = useState<Boolean>(false);
 
 
   useEffect(()=>{
     window.addEventListener('resize', windowResizeHandler);
-
+    
+    setRenderChildren(true);
     return () => {
       window.removeEventListener('resize', windowResizeHandler);
     }
@@ -38,28 +40,36 @@ function App() {
 
   return (
     <>
-      <ConsoleLogApp/>
-      <BrowserRouter>
-        <div id='navbar'>
-          <ul>
-            <li><Link to='/'>home</Link></li>
-            <li><Link to='/wave'>wave</Link></li>
-            <li><Link to='/mouseTracker'>mouse tracker</Link></li>
-            <li><Link to='/solarSystem'>solar system (not completed)</Link></li>
-            <li><Link to='/react-pixi-1'>react pixi 1 (not completed)</Link></li>
-          </ul>
-        </div>
+      {
+        renderChildren 
+        ? 
+        <>
+          <ConsoleLogApp/>
+          <BrowserRouter>
+            <div id='navbar'>
+              <ul>
+                <li><Link to='/'>home</Link></li>
+                <li><Link to='/wave'>wave</Link></li>
+                <li><Link to='/mouseTracker'>mouse tracker</Link></li>
+                <li><Link to='/solarSystem'>solar system (not completed)</Link></li>
+                <li><Link to='/react-pixi-1'>react pixi 1 (not completed)</Link></li>
+              </ul>
+            </div>
 
-        <div id='content'>
-          <Routes>
-            <Route path='/' element={<RootApp windowSize={windowSize} />}></Route>
-            <Route path='/wave' element={<WaveApp windowSize={windowSize}/>} ></Route>
-            <Route path='/mouseTracker' element={<MouseTrackerApp windowSize={windowSize}/>}></Route>
-            <Route path='/solarSystem' element={<SolarSystemApp windowSize={windowSize}/>}></Route>
-            <Route path='/react-pixi-1' element={<ReactPIXIExample1App windowSize={windowSize}/>}></Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
+            <div id='content'>
+              <Routes>
+                <Route path='/' element={<RootApp windowSize={windowSize} />}></Route>
+                <Route path='/wave' element={<WaveApp windowSize={windowSize}/>} ></Route>
+                <Route path='/mouseTracker' element={<MouseTrackerApp windowSize={windowSize}/>}></Route>
+                <Route path='/solarSystem' element={<SolarSystemApp windowSize={windowSize}/>}></Route>
+                <Route path='/react-pixi-1' element={<ReactPIXIExample1App windowSize={windowSize}/>}></Route>
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </>
+        :
+        "loading..."
+      }
     </>
   );
 }
